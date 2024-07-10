67de73e5a6fb1b96673e629644944f9767cb1382
Daniel Henrique <medanielsantos@gmail.com>
2024-07-10 08:47:27 -0300

    fix: migration

--- internal/pgstore/migrations/002_create_participants_table.sql
+++ internal/pgstore/migrations/002_create_participants_table.sql
@@ -1,12 +1,14 @@
-CREATE TABLE IF NOT EXISTS trips (
-    "id" uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
-    "trip_id" uuid NOT NULL,
-    "email" varchar(255) NOT NULL,
-    "is_confirmed" boolean NOT NULL DEFAULT false,
+CREATE TABLE IF NOT EXISTS participants (
+    "id"            uuid            PRIMARY KEY NOT NULL    DEFAULT gen_random_uuid(),
+    "trip_id"       uuid                        NOT NULL,
+    "email"         VARCHAR(255)                NOT NULL,
+    "is_confirmed"  BOOLEAN                     NOT NULL    DEFAULT FALSE,
 
-    FOREIGN KEY ("trip_id") REFERENCES trips("id")
+    FOREIGN KEY (trip_id) REFERENCES trips(id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
-    );
+);
+
 ---- create above / drop below ----
+
 DROP TABLE IF EXISTS participants;
